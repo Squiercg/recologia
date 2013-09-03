48cf1f3c35223816176f8a49af7cf471bb0de18e
Augusto Ribas <ribas.aca@gmail.com>
2013-09-03 18:20:15 -0400

    Post do metropolis para distribuição normal

--- metropolisnormal.r
+++ metropolisnormal.r
@@ -25,21 +25,54 @@ mtau<-a+n/2
 # Inicio da cadeia #
 ####################
 
-mu<-runif(1,0,10)
-tau<-runif(1,0,10)
+mu<-0.1
+tau<-0.1
 nsim<-5000
 
 ##########
 # Cadeia #
 ##########
-cadeia.mu<-rep(0,nsim)
-cadeia.tau<-rep(0,nsim)
+cadeia.mu<-c(mu,rep(0,nsim-1))
+cadeia.tau<-c(tau,rep(0,nsim-1))
 A<-0
+
+
+#Valores para avaliar a maxima verossimilhança
+valores<-seq(-5,15,by=0.1)
+likelihood<-vector()
+
+for(i in 1:length(valores)) {
+    likelihood[i]<-prod(dnorm(dados,valores[i],sqrt(2),log=F))
+    }
+
+plot(likelihood~valores,type="b",frame=F,ylab="likelihood")
+
+loglikelihood<-vector()
+
+for(i in 1:length(valores)) {
+    loglikelihood[i]<-sum(dnorm(dados,valores[i],sqrt(2),log=T))
+    }
+
+plot(loglikelihood~valores,type="b",frame=F,ylab="loglikelihood")
+
+exp(loglikelihood)[100]==likelihood[100]
+all.equal(exp(loglikelihood)[100],likelihood[100])
+
+max(likelihood)
+
+which(likelihood==max(likelihood))
+valores[101]
+mean(dados)
+valores[102]
+
+max(loglikelihood)
+which(loglikelihood==max(loglikelihood))
+
 
 ############
 #   MCMC   #
 ############
-for (t in 1:nsim){
+for (t in 2:nsim){
     mus<-mu+0.3*rt(1,3)
 
     r<- (sum(dnorm(dados,mus,1/sqrt(tau),log=T) - dnorm(dados,mu,1/sqrt(tau),log=T)) +
@@ -51,7 +84,9 @@ for (t in 1:nsim){
         }
 
     cadeia.mu[t]<-mu
-    tau<-cadeia.tau[t]<-rgamma(1,mtau,b+sum((dados-mu)^2)/2)
+
+    tau<-rgamma(1,mtau,b+sum((dados-mu)^2)/2)
+    cadeia.tau[t]<-tau
 }
 
 ###########
@@ -65,15 +100,20 @@ plot(seq(1,nsim) ,cadeia.mu[1:nsim],type="l",xlab="Iteration", ylab="mu",frame=F
 main="Média",col=2,lty=1,ylim=c(0,10))
 abline(h=mu.real)
 plot(seq(1,nsim) ,cadeia.tau[1:nsim],type="l",xlab="Iteration", ylab="mu",frame=F,
-main="Desvio",col=2,lty=1,ylim=c(0,1))
+main="Tau",col=2,lty=1,ylim=c(0,1))
 abline(h=1/sigma2.real)
 
 par(mar=c(2,2,2,2),font=3,cex=0.7)
 
-hist(cadeia.mu[1:100],xlab="",ylab="Frequência",main="",cex=0.7,xlim=c(4,6))
-hist(cadeia.mu[2000:2500],xlab="",ylab="Frequência",main="",cex=0.7,xlim=c(4,6))
-hist(cadeia.mu[4500:5000],xlab="",ylab="Frequência",main="",cex=0.7,xlim=c(4,6))
-hist(cadeia.tau[1:500],xlab="",ylab="Frequência",main="",cex=0.7,xlim=c(0,1))
-hist(cadeia.tau[2000:2500],xlab="",ylab="Frequência",main="",cex=0.7,xlim=c(0,1))
-hist(cadeia.tau[4500:5000],xlab="",ylab="Frequência",main="",cex=0.7,xlim=c(0,1))
+hist(cadeia.mu[1:100],xlab="",ylab="Frequência",main="",cex=0.7,xlim=c(3,6),breaks=seq(0,6,by=0.02))
+hist(cadeia.mu[2000:2500],xlab="",ylab="Frequência",main="",cex=0.7,xlim=c(3,6),breaks=seq(3,6,by=0.02))
+hist(cadeia.mu[4500:5000],xlab="",ylab="Frequência",main="",cex=0.7,xlim=c(3,6),breaks=seq(3,6,by=0.02))
+hist(cadeia.tau[1:500],xlab="",ylab="Frequência",main="",cex=0.7,xlim=c(0,1),breaks=seq(0,1,by=0.01))
+hist(cadeia.tau[2000:2500],xlab="",ylab="Frequência",main="",cex=0.7,xlim=c(0,1),breaks=seq(0,1,by=0.01))
+hist(cadeia.tau[4500:5000],xlab="",ylab="Frequência",main="",cex=0.7,xlim=c(0,1),breaks=seq(0,1,by=0.01))
 
+plot(cadeia.mu[1:500],sqrt(1/cadeia.tau[1:500]),type="b",frame=F,xlab="Média",ylab="Desvio Padrão",cex=0.5,pch=19,
+     xlim=c(0,6),ylim=c(0,6))
+library(MASS)
+contornos<-kde2d(cadeia.mu[1:500],sqrt(1/cadeia.tau[1:500]))
+contour(contornos,add=T,lwd=2,nlevels = 6,col=rev(heat.colors(6)))
