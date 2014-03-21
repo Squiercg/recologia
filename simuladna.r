6b33ace99312bd0f1614731d6f628518bc7c8605
Augusto Ribas <ribas.aca@gmail.com>
2014-03-21 14:11:33 -0400

    Simulando dna, post novo no blog

--- simuladna.r
+++ simuladna.r
@@ -1,6 +1,8 @@
 sample(c("Cara","Coroa"),30,rep=TRUE,prob=c(0.5,0.5))
 
-sample(c("A","G","C","T"),30,rep=TRUE,prob=c(0.1,0.4,0.4,0.1))
+sample(c("A","G","C","T"),30,rep=TRUE,prob=c(0.25,0.25,0.25,0.25))
+
+
 
 markov1 <- function(x,P,n) {
     seq <- x
@@ -12,7 +14,9 @@ markov1 <- function(x,P,n) {
 
 P <- matrix(c(1/6,5/6,0.5,0.5), 2, 2, byrow=TRUE)
 
-rownames(P) <- colnames(P) <- StateSpace <- x <- c(1,2)
+rownames(P)<-c(1,2)
+colnames(P)<-c(1,2)
+x<-c(1,2)
 
 markov1(x,P,30)
 
@@ -40,30 +44,33 @@ x <- markov2(StateSpace,P,pi0,1000)
 head(x)
 table(x)
 
-pi0 <- c(1/4,1/4,1/4,1/4)
 
 P <- matrix(c(.01,.01,.01,.97,
 .01,.01,.01,.97,
 .01,.01,.01,.97,
 .01,.28,.01,0.70),4,4,byrow=T)
-
 rownames(P) <- colnames(P) <- StateSpace <- c("a","c","g","t")
-
 x <- markov2(StateSpace,P,pi0,30000)
 
 #install.packages("seqinr")
 library(seqinr)
 table(getTrans(x))
 
-
 nr <- count(x,2)
-names(nr)
+nr
+
 A <- matrix(NA,4,4)
+rownames(A) <- colnames(A) <- c("a","c","g","t")
 A[1,1]<-nr["aa"]; A[1,2]<-nr["ag"]; A[1,3]<-nr["ac"]; A[1,4]<-nr["at"]
 A[2,1]<-nr["ga"]; A[2,2]<-nr["gg"]; A[2,3]<-nr["gc"]; A[2,4]<-nr["gt"]
 A[3,1]<-nr["ca"]; A[3,2]<-nr["cg"]; A[3,3]<-nr["cc"]; A[3,4]<-nr["ct"]
 A[4,1]<-nr["ta"]; A[4,2]<-nr["tg"]; A[4,3]<-nr["tc"]; A[4,4]<-nr["tt"]
+
+A
+
 rowsumA <- apply(A, 1, sum)
 Phat <- sweep(A, 1, rowsumA, FUN="/")
 rownames(Phat) <- colnames(Phat) <- c("a","g","c","t")
+
+P
 round(Phat,3)
