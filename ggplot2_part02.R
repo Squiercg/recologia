a3dc2091c499c933e35ef50231b70b942026948c
Augusto Ribas <ribas.aca@gmail.com>
2016-05-17 14:41:06 -0400

    Atualizando

--- ggplot2_part02.R
+++ ggplot2_part02.R
@@ -1,30 +1,35 @@
-install.packages("ggplot2y")
+install.packages("ggplot2")
+library("ggplot2")
 
-(105*50000000/4469000)/60
-# Very basic bar graph
-ggplot(data=dat, aes(x=time, y=total_bill)) +
-    geom_bar(stat="identity")
+dados<-data.frame(preditor=1:20,resposta=runif(20,5,10),grupo=rep(1:4,each=5),linha=runif(20,8,12))
 
-
-# Map the time of day to different fill colors
-ggplot(data=dat, aes(x=time, y=total_bill, fill=time)) +
-    geom_bar(stat="identity")
-
-## This would have the same result as above
-# ggplot(data=dat, aes(x=time, y=total_bill)) +
-#    geom_bar(aes(fill=time), stat="identity")
+## Gráfico de barras
+ggplot(data=dados, aes(x=preditor, y=resposta)) + geom_bar(stat="identity")
 
 
-# Add a black outline
-ggplot(data=dat, aes(x=time, y=total_bill, fill=time)) +
-    geom_bar(colour="black", stat="identity")
+## Separando por grupos
+ggplot(data=dados, aes(x=preditor, y=resposta,fill=grupo)) + geom_bar(stat="identity")
+##ggplot(data=dados, aes(x=preditor, y=resposta)) + geom_bar(aes(fill=grupo),stat="identity")
 
 
-# No legend, since the information is redundant
-ggplot(data=dat, aes(x=time, y=total_bill, fill=time)) +
-    geom_bar(colour="black", stat="identity") +
-    guides(fill=FALSE)
+## Adicionando um contorno preto, e retirando a legenda
+ggplot(data=dados, aes(x=preditor, y=resposta,fill=grupo)) + geom_bar(colour="black",stat="identity") + guides(fill=FALSE)
 
-ggplot(data=datn, aes(x=dose, y=length, group=supp, colour=supp)) +
-    geom_line() +
-    geom_point()
+
+##Gráfico de linhas
+ggplot(data=dados, aes(x=preditor, y=linha)) + geom_line() + guides(fill=FALSE)
+ggplot(data=dados, aes(x=preditor, y=linha)) + geom_line() + geom_point() + guides(fill=FALSE)
+
+
+##Combinando os dois
+ggplot(data=dados, aes(x=preditor, y=resposta,fill=grupo)) + geom_bar(colour="black",stat="identity") + guides(fill=FALSE) +
+    geom_line(data=dados, aes(x=preditor, y=linha)) + geom_point(data=dados, aes(x=preditor, y=linha))
+
+
+
+##Transformando em uma figura circular
+ggplot(data=dados, aes(x=preditor, y=resposta,fill=grupo)) + geom_bar(colour="black",stat="identity") + guides(fill=FALSE) +
+    geom_line(data=dados, aes(x=preditor, y=linha)) + geom_point(data=dados, aes(x=preditor, y=linha))+coord_polar()
+
+ggplot(data=dados, aes(x=preditor, y=linha)) + geom_line() + geom_point() + guides(fill=FALSE) + coord_polar()
+ggplot(data=dados, aes(x=preditor, y=linha)) + geom_line() + geom_point() + guides(fill=FALSE) + coord_polar() + ylim(0,12)
